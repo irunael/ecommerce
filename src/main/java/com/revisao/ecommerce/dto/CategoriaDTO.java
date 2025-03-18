2d465bfb39ad67cdaec716f3f0e94019620de9ee
irunael <pha.alvarez2020@gmail.com>
2025-03-18 10:26:19 -0300

    vai dar certo

---
+++ src/main/java/com/revisao/ecommerce/dto/CategoriaDTO.java
@@ -0,0 +1,47 @@
+package com.revisao.ecommerce.dto;
+
+import com.revisao.ecommerce.entities.Categoria;
+
+public class CategoriaDTO {
+	
+	private Long id;
+	private String nome;
+	
+	
+	public CategoriaDTO(Long id, String nome) {
+		this.id = id;
+		this.nome = nome;
+	}
+	
+	public CategoriaDTO(Categoria entity) {
+		id = entity.getId();
+		nome = entity.getNome();
+	}
+	
+	
+	public CategoriaDTO() {
+		
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
